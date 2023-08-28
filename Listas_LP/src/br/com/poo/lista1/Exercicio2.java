fa31a64a7f861fb318184c643142d76bf36bd38e
philippusv <felipebalbinoribeiro@gmail.com>
2023-08-28 18:47:40 -0300

    git ignored settings

---
+++ Listas_LP/src/br/com/poo/lista1/Exercicio2.java
@@ -0,0 +1,28 @@
+package br.com.poo.lista1;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		String idade;
+		String somaIdade;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("Qual é a idade do colega "+ i + " ? ");
+			idade = sc.next();
+			somaIdade = somaIdade + idade;
+			
+		}
+		
+		System.out.println("Qual é a sua idade? ");
+		idade = sc.next();
+		somaIdade = somaIdade + idade;
+		
+		System.out.println("O tempo de vida meu e dos meus colegas é " + somaIdade);
+
+	}
+
+}
