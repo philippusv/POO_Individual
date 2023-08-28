a94a683b7e94766ea371516dc2169ae77b5d5349
philippusv <felipebalbinoribeiro@gmail.com>
2023-08-28 20:09:07 -0300

    renomeando pasta lista_lp

--- Exercicios_Java/src/br/com/poo/lista1/Exercicio2.java
+++ Exercicios_Java/src/br/com/poo/lista1/Exercicio2.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Exercicio2 {
 
 	public static void main(String[] args) {
-		String idade;
-		String somaIdade;
+		int idade;
+		int somaIdade;
 		somaIdade = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		for(int i = 1; i <= 5; i++) {
 			System.out.println("Qual é a idade do colega "+ i + " ? ");
-			idade = sc.next();
+			idade = sc.nextInt();
 			somaIdade = somaIdade + idade;
 			
 		}
 		
 		System.out.println("Qual é a sua idade? ");
-		idade = sc.next();
+		idade = sc.nextInt();
 		somaIdade = somaIdade + idade;
 		
 		sc.close();
--- Exercicios_Java/src/br/com/poo/lista1/Exercicio3.java
+++ Exercicios_Java/src/br/com/poo/lista1/Exercicio3.java
@@ -3,7 +3,7 @@ package br.com.poo.lista1;
 public class Exercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
